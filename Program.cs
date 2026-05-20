bcbd69422818d69e96380c5d9b97c6b0ddbc0d67
Georgy379 <125569634+Georgy379@users.noreply.github.com>
2026-05-20 19:11:32 +0300

    file add

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HelloApp
+{
+class Program
+{
+static void Main(string[] args)
+{
+    Console.Write("Введите своё имя");
+    string name = Console.ReadLine();   //ConsoleApp1
+        Console.WriteLine($"Привет {name}"); //ConsoleApp1
+        Console.WriteLine($"Очень рады вас видеть!!!");
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write(i);
+        }
+        i = i*3
+        Console.ReadKey();
+        }
+    }
+}
